db77eeaa9ae2f46e3c660914b8eba6eb22dd0e9b
Chitranshi1~ <chitranshigourana@gmail.com>
2024-06-15 00:56:48 +0530

    Updated dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
-require("dotenv").config();
+const dotenv = require("dotenv");
+dotenv.config();
 const connectDB = require("./config/db_config");
 const color = require("colors");
 const path = require("path");
